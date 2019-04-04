02453f2a6a4155eb7902767b13aefeeec9f96e28
BadiGamer <48218842+BadiGamer@users.noreply.github.com>
2019-04-04 21:55:40 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,7 +35,7 @@ var ti={}
 
 client.on('ready', function(){
     var ms = 60000 ;
-    var setGame = ['Hack by Mahdi','	Hack BY Mahdi'];
+    var setGame = ['$help','	$help'];
     var i = -1;
     var j = 0;
     setInterval(function (){
